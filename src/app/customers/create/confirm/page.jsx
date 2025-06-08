5a014d01539260e697a42566d71f0402ed56c220
taku0875 <always092471@gmail.com>
2025-06-08 23:30:13 +0900

    Update page.jsx

--- src/app/customers/create/confirm/page.jsx
+++ src/app/customers/create/confirm/page.jsx
@@ -1,33 +1,10 @@
-"use client";
-import OneCustomerInfoCard from "@/app/components/one_customer_info_card.jsx";
-import fetchCustomer from "./fetchCustomer";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
+import ConfirmPage from "./ConfirmClient";
 
-export default function ConfirmPage() {
-  const router = useRouter();
-  const customer_id = useSearchParams().get("customer_id");
-  const [customer, setCustomer] = useState(null);
-
-  useEffect(() => {
-    const fetchAndSetCustomer = async () => {
-      const customerData = await fetchCustomer(customer_id);
-      setCustomer(customerData);
-    };
-    fetchAndSetCustomer();
-  }, []);
-
+export default function Page() {
   return (
-    <>
-      <div className="card bordered bg-white border-blue-200 border-2 max-w-sm m-4">
-        <div className="alert alert-success p-4 text-center">
-          正常に作成しました
-        </div>
-        <OneCustomerInfoCard {...customer} />
-        <button onClick={() => router.push("./../../customers")}>
-          <div className="btn btn-primary m-4 text-2xl">戻る</div>
-        </button>
-      </div>
-    </>
+    <Suspense fallback={<div>読み込み中...</div>}>
+      <ConfirmPage />
+    </Suspense>
   );
 }
