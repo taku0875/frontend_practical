f6171091702f07d667e86d3a9195c68ea0896d46
taku0875 <always092471@gmail.com>
2025-06-08 23:31:33 +0900

    Create confirmclinent.jsx

---
+++ src/app/customers/create/confirmclinent.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import OneCustomerInfoCard from "@/app/components/one_customer_info_card.jsx";
+import fetchCustomer from "./fetchCustomer";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function ConfirmPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const customer_id = searchParams.get("customer_id");
+  const [customer, setCustomer] = useState(null);
+
+  useEffect(() => {
+    if (!customer_id) return;
+    const fetchAndSetCustomer = async () => {
+      try {
+        const customerData = await fetchCustomer(customer_id);
+        setCustomer(customerData[0]);
+      } catch (error) {
+        console.error("Failed to fetch:", error);
+      }
+    };
+    fetchAndSetCustomer();
+  }, [customer_id]);
+
+  return (
+    <div className="card bordered bg-white border-blue-200 border-2 max-w-sm m-4">
+      <div className="alert alert-success p-4 text-center">正常に作成しました</div>
+      {customer && <OneCustomerInfoCard {...customer} />}
+      <button onClick={() => router.push("./../../customers")}>
+        <div className="btn btn-primary m-4 text-2xl">戻る</div>
+      </button>
+    </div>
+  );
+}
