9c1de22a66bc6bd6a7d9635257b3328c0bc1aa27
taku0875 <always092471@gmail.com>
2025-06-09 06:20:53 +0900

    Update fetchCustomer.js

--- src/app/customers/create/confirm/fetchCustomer.js
+++ src/app/customers/create/confirm/fetchCustomer.js
@@ -1,10 +1,7 @@
 export default async function fetchCustomer(id) {
-  const res = await fetch(
-    process.env.NEXT_PUBLIC_API_ENDPOINT + `/customers?customer_id=${id}`,
-    {
-      cache: "no-cache",
-    }
-  );
+  const url = process.env.NEXT_PUBLIC_API_ENDPOINT + `/customers?customer_id=${id}`;
+  console.log("✅ 呼び出しURL:", url); // ← 追加
+  const res = await fetch(url, { cache: "no-cache" });
   if (!res.ok) {
     throw new Error("Failed to fetch customer");
   }
