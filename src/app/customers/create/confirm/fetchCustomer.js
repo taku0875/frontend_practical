ae8cd9802f49e989ad52796dc5db48df6295acf0
taku0875 <always092471@gmail.com>
2025-06-10 20:31:09 +0900

    Delete src/app directory

--- src/app/customers/create/confirm/fetchCustomer.js
+++
@@ -1,9 +0,0 @@
-export default async function fetchCustomer(id) {
-  const url = process.env.NEXT_PUBLIC_API_ENDPOINT + `/customers?customer_id=${id}`;
-  console.log("✅ 呼び出しURL:", url); // ← 追加
-  const res = await fetch(url, { cache: "no-cache" });
-  if (!res.ok) {
-    throw new Error("Failed to fetch customer");
-  }
-  return res.json();
-}
